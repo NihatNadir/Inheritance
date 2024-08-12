167f076c51fc33e16cab7f204229f76410843315
NIHAT <NIHAT@DESKTOP-3F2LN5P>
2024-08-12 23:12:53 +0300

    Add project files.

---
+++ Inheritance/BasePerson.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class BasePerson
+    {
+
+        private string name; // BasePerson classından name field
+        private string surName; // BasePerson classından surName field
+
+        public string Name // BasePerson classından Name property
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string SurName // BasePerson classından SurName property
+        {
+            get
+            {
+                return surName;
+            }
+            set
+            {
+                surName = value;
+            }
+        }
+
+        public void PrintPerson() // BasePerson için metot
+        {
+            Console.WriteLine($"Hoşgeldiniz {name} {surName}");
+        }
+
+    }
+
+    public class Student : BasePerson // BasePerson'a ait özellikleri Student'a inheritance özellliği ile aktarıyoruz.
+    {
+        private int studentNumber; // Student classından studentNumber field
+
+        public int StudentNumber // Student classından StudentNumber property
+        {
+            get
+            {
+                return studentNumber;
+            }
+            set
+            {
+                studentNumber = value;
+            }
+        }
+
+        public void PrintStudent() // Student için metot
+        {
+            PrintPerson(); // BasePerson'dan alınan metot
+            Console.WriteLine($"Öğrenci numaranız {studentNumber}\n");
+            
+        }
+    }
+
+    public class Teacher : BasePerson // BasePerson'a ait özellikleri Teacher'a inheritance özellliği ile aktarıyoruz.
+    {
+        private int salary; // Student classından salary field
+
+        public int Salary // Student classından Salary property
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+
+        public void PrintTeacher() // Teacher için metot
+        {
+            PrintPerson(); // BasePerson'dan alınan metot
+            Console.WriteLine($"Öğretmen maaşınız {salary}\n");
+        }
+    }
+
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Security.Cryptography.X509Certificates;
+namespace Inheritance
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // BasePerson classından person1 nesnesi üretiyoruz.
+
+            /*
+            BasePerson person1 = new BasePerson();
+            person1.Name = "Ali";
+            person1.SurName = "Yalçın";
+            person1.PrintPerson();
+            */
+
+            // Student classından student1 nesnesi üretiyoruz.
+
+            Student student1 = new Student();
+            student1.Name = "Mehmet";
+            student1.SurName = "Göçer";
+            student1.StudentNumber = 148;
+            Console.WriteLine("Öğrenci 1");
+            student1.PrintStudent();
+
+            // Student classından student2 nesnesi üretiyoruz.
+
+            Student student2 = new Student();
+            student2.Name = "Zeynep";
+            student2.SurName = "Adıgüzel";
+            student2.StudentNumber = 440;
+            Console.WriteLine("Öğrenci 2");
+            student2.PrintStudent();
+
+            // Teacher classından teacher1 nesnesi üretiyoruz.
+
+            Teacher teacher1 = new Teacher();
+            teacher1.Name = "Zehra";
+            teacher1.SurName = "Güneş";
+            teacher1.Salary = 50000;
+            Console.WriteLine("Öğretmen 1");
+            teacher1.PrintTeacher();
+
+            // Teacher classından teacher2 nesnesi üretiyoruz.
+
+            Teacher teacher2 = new Teacher();
+            teacher2.Name = "Cengiz";
+            teacher2.SurName = "Kaya";
+            teacher2.Salary = 30000;
+            Console.WriteLine("Öğretmen 2");
+            teacher2.PrintTeacher();
+        }
+    }
+}
